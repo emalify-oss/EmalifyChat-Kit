d3ba21e6953ea7b43779ee337e06ed3e989f2acc
Dinox869 <35877345+Dinox869@users.noreply.github.com>
2020-03-13 11:05:57 +0300

    Update MessagePojo.java

--- roamtechsdk/src/main/java/com/example/roamtechsdk/Model/MessagePojo.java
+++ roamtechsdk/src/main/java/com/example/roamtechsdk/Model/MessagePojo.java
@@ -20,11 +20,132 @@ public class MessagePojo  {
 
     String contact_id;
     String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
+
+
+    public String getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(String isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    String type;
+
+    public Long getMeta() {
+        return meta;
+    }
+
+    public void setMeta(Long meta) {
+        this.meta = meta;
+    }
+
+    Long meta;
+    String email;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCustomer_account() {
+        return customer_account;
+    }
+
+    public void setCustomer_account(String customer_account) {
+        this.customer_account = customer_account;
+    }
+
+    String author;
+    String customer_account;
+    String date;
+
+    public Long getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Long created_at) {
+        this.created_at = created_at;
+    }
+
+    Long created_at;
+
+    public Long getMessage_id() {
+        return message_id;
+    }
+
+    public void setMessage_id(Long message_id) {
+        this.message_id = message_id;
+    }
+
+    Long message_id;
+    String isAdmin;
+
+    public String getContact_id() {
+        return contact_id;
+    }
+
+    public void setContact_id(String contact_id) {
+        this.contact_id = contact_id;
+    }
+
+    public String getSetID() {
+        return setID;
+    }
+
+    public void setSetID(String setID) {
+        this.setID = setID;
+    }
+
     String setID;
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
     String channel;
-
-
-
 
     public String getImage() {
         return image!=null?image.trim():null;
@@ -115,7 +236,7 @@ public class MessagePojo  {
         Author a=new Author();
         a.setName(name);
         a.setId(id);
-        a.setAvatar("http://none.com");
+        a.setAvatar(getImage());
         m.setUser(a);
         m.setImage(getImage());
         return m;
